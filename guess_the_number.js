63e6e0669198c6e729b3e4a25d8efecfcc1b1c6b
varrapavani <pavani21057cm051@gmail.com>
2024-11-20 19:29:22 +0530

    Update guess_the_number.js

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,18 +1,24 @@
 // Assume rangeStart and rangeEnd are always greater than 0.
 // rangeStart is always less than rangeEnd.
-function getUserGuess(secretNumber, maxAttempts) {
+
+function getUserNumber() {
+  const userNumber = +prompt('Guess a Secret Number : ');
+  
+  if (userNumber + '' === 'NaN') {
+    console.log('Invalid input! Please enter a number.');
+    getUserNumber();
+  }
+  return userNumber;
+}
+
+function isUserNumber(secretNumber, maxAttempts) {
   console.log('Take a guess! (Remaining attempts: ' + maxAttempts + ')');
   
   if (maxAttempts === 0) {
     console.log("Oh no! You've used all your attempts. Better luck next time!");
     return '';
   }
-  const userNumber = +prompt('Guess a Secret Number : ');
-  
-  if (userNumber + '' === 'NaN') {
-    console.log('Invalid input! Please enter a number.');
-    getUserGuess(secretNumber, maxAttempts);
-  }
+  const userNumber = getUserNumber();
 
   if (userNumber > secretNumber) {
     console.log(userNumber + 'Too high! Try a smaller number.');
@@ -24,14 +30,14 @@ function getUserGuess(secretNumber, maxAttempts) {
     console.log("Bravo! You've nailed it. The number was " + secretNumber + '!');
     return '';
   }
-  getUserGuess(secretNumber, maxAttempts - 1);
+  isUserNumber(secretNumber, maxAttempts - 1);
 }
 
 function playGame(rangeStart, rangeEnd, maxAttempts) {
   const range = rangeEnd - rangeStart;
   const secretNumber = Math.round(Math.random() * range) + rangeStart;
-
-  getUserGuess(secretNumber, maxAttempts);
+  console.log(secretNumber);
+  isUserNumber(secretNumber,maxAttempts);
 
   if (confirm('Do you want to play again')) {
     playGame(rangeStart, rangeEnd, maxAttempts)
@@ -47,4 +53,4 @@ function startGame(rangeStart, rangeEnd, maxAttempts) {
   playGame(rangeStart, rangeEnd, maxAttempts);
 }
 
-startGame(100, 200, 6);
+startGame(10, 20, 6);
