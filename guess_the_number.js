46e95e1df361227117b7f73a7d65c8c8bde5733f
varrapavani <pavani21057cm051@gmail.com>
2024-11-21 09:58:08 +0530

    Update guess_the_number.js

--- guess_the_number.js
+++ guess_the_number.js
@@ -1,43 +1,53 @@
 // Assume rangeStart and rangeEnd are always greater than 0.
 // rangeStart is always less than rangeEnd.
+function isEqualToSecret(userNumber, secretNumber) {
+  if (userNumber > secretNumber) {
+    console.log(userNumber + 'Too high! Try a smaller number.');
+    return false;
+  }
+
+  if (userNumber < secretNumber) {
+    console.log(userNumber + 'Too low! Try a higher number.');
+    return false;
+  }
+
+  if (userNumber === secretNumber) {
+    console.log("Bravo! You've nailed it. The number was " + secretNumber + '!');
+    return true;
+  }
+}
 
 function getUserNumber() {
   const userNumber = +prompt('Guess a Secret Number : ');
-  
+
   if (userNumber + '' === 'NaN') {
     console.log('Invalid input! Please enter a number.');
-    getUserNumber();
+    return getUserNumber();
   }
   return userNumber;
 }
 
-function isUserNumber(secretNumber, maxAttempts) {
+function takeUserGuesses(secretNumber, maxAttempts) {
   console.log('Take a guess! (Remaining attempts: ' + maxAttempts + ')');
-  
+
   if (maxAttempts === 0) {
     console.log("Oh no! You've used all your attempts. Better luck next time!");
     return '';
   }
   const userNumber = getUserNumber();
+  const isSecretNumber = isEqualToSecret(userNumber, secretNumber);
 
-  if (userNumber > secretNumber) {
-    console.log(userNumber + 'Too high! Try a smaller number.');
+  if (!isSecretNumber) {
+    return takeUserGuesses(secretNumber, maxAttempts - 1);
   }
-  if (userNumber < secretNumber) {
-    console.log(userNumber + 'Too low! Try a higher number.');
-  }
-  if (userNumber === secretNumber) {
-    console.log("Bravo! You've nailed it. The number was " + secretNumber + '!');
-    return '';
-  }
-  isUserNumber(secretNumber, maxAttempts - 1);
+  return '';
 }
 
 function playGame(rangeStart, rangeEnd, maxAttempts) {
   const range = rangeEnd - rangeStart;
   const secretNumber = Math.round(Math.random() * range) + rangeStart;
   console.log(secretNumber);
-  isUserNumber(secretNumber,maxAttempts);
+  takeUserGuesses(secretNumber, maxAttempts);
 
   if (confirm('Do you want to play again')) {
     playGame(rangeStart, rangeEnd, maxAttempts)
